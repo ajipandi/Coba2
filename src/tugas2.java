e33cb93e8af9052e89706cc6d16d2b0d9dcbc2fe
ajipandi <43037895+ajipandi@users.noreply.github.com>
2018-09-19 19:02:44 +0700

    Add files via upload

---
+++ src/tugas2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nuser
+ */
+public class tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int n = 5;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if (j==0 || j==(n-1) || i==0 || i==(n-1)){
+                System.out.print("#");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.
+                println("");
+}
+    }
+    
+}
